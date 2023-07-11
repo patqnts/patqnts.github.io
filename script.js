bb6f51b735b79ff65a1b467eccc940b629e0df94
John Patrick Quintos <113108715+patqnts@users.noreply.github.com>
2023-07-11 18:42:43 +0800

    Projects update

--- script.js
+++ script.js
@@ -52,3 +52,26 @@ window.addEventListener('scroll', function() {
 });
 
 
+var slideshowItems = document.getElementsByClassName('slideshow-items');
+  var currentIndex = 0;
+  
+  setInterval(function() {
+    slideshowItems[currentIndex].style.opacity = 0;
+    currentIndex = (currentIndex + 1) % slideshowItems.length;
+    slideshowItems[currentIndex].style.opacity = 1;
+  }, 3000);
+
+  var slideshowItems1 = document.getElementsByClassName('slideshow-item');
+  var currentIndexs = 0;
+  
+  setInterval(function() {
+    slideshowItems1[currentIndexs].style.opacity = 0;
+    currentIndexs = (currentIndexs + 1) % slideshowItems1.length;
+    slideshowItems1[currentIndexs].style.opacity = 1;
+  }, 3000);
+
+
+
+  
+
+  
